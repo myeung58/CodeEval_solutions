aa1f956b143257bb452bc4631ceff39b93c30077
Michael Yeung <myeung58@Michaels-MacBook-Pro.local>
2014-10-19 21:41:49 -0700

    data recovery

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Completed Challenges
 Easy:
 - Prime Palindrome (Biggest prime palindrome < 1000)
 - Multiplication Tables (Print out the grade school multiplication table upto 12*12)
+- Data Recovery (Reconstruct a sentence using hints)
+- Sum of Primes (Sum of first 1000 primes)
 
 Moderate:
 - Longest Lines (Finding the 'N' longest lines within a file)
---
+++ data_recovery.rb
@@ -0,0 +1,18 @@
+def recover(words, sequence)
+  result = []
+  missing_num = 0
+  sequence.map! { |n| n.to_i }
+  (1..sequence.length + 1).to_a.each do |n|
+    missing_num = n unless sequence.include?(n)
+  end
+  words[0...sequence.length].each_with_index do |word, index|
+    result[sequence[index].to_i - 1] = word
+  end
+  result[missing_num - 1] = words[-1]
+  result.join(" ")
+end
+
+content = File.readlines(ARGV[0]).each do |line|
+  line = line.chomp.split(";")
+  puts recover(line[0].split(" "), line[1].split(" "))
+end
