9d099f006c65983e40d65e3bd5765e15e645f9b9
myeung58 <myeung58@gmail.com>
2014-11-10 12:26:15 -0800

    prime numbers

---
+++ prime_numbers.rb
@@ -0,0 +1,18 @@
+def prime(n)
+  result = []
+  (1..n).to_a.each do |num|
+    divisible = false
+    (2..num-1).to_a.each do |i|
+      divisible = true if num % i == 0
+    end
+    result << num if !divisible
+  end
+  nums = (1..n).to_a
+
+  result
+end
+
+prime(20)
+# File.open(ARGV[0]).each_line do |line|
+#   puts prime(line.chomp.to_i).join(",")
+# end
